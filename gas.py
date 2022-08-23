e28b35ca4c6a188287f7b658d40a9705ab256e49
mohbarati <mohbarati14@gmail.com>
2022-08-23 14:22:23 +0430

    improvement

--- gas.py
+++ gas.py
@@ -1,7 +1,7 @@
+from math import sqrt
+
 import pygame
 from pygame.locals import *
-from math import sqrt
-from time import sleep
 
 pygame.init()
 sc = pygame.display.set_mode((600, 600))
@@ -14,7 +14,10 @@ sc.fill(black)
 
 
 class ball:
-    k = 1
+    """Making instances of a ball and its atributes
+    """
+
+    k = 1000
 
     def __init__(self, x, y, r, color, vx, vy, m):
         self.x = x
@@ -26,7 +29,7 @@ class ball:
         self.m = m
 
     def __distance(self, x, y):
-        return sqrt(x**2 + y**2)
+        return sqrt(x ** 2 + y ** 2)
 
     def draw_ball(self):
         pygame.draw.circle(sc, self.color, (self.x, self.y), self.r)
@@ -35,13 +38,7 @@ class ball:
         pygame.draw.circle(sc, black, (self.x, self.y), self.r)
 
     def collision_detection(self, new):
-        if (
-            self.__distance(
-                (self.x) - (new.x),
-                (self.y) - (new.y),
-            )
-            <= self.r + new.r
-        ):
+        if self.__distance((self.x) - (new.x), (self.y) - (new.y),) <= self.r + new.r:
             return True
         return False
 
@@ -49,64 +46,99 @@ class ball:
         del_x = self.x - new.x
         del_y = self.y - new.y
         distance = self.__distance(del_x, del_y)
-        f = -self.k * (distance - (self.r + new.r))
+        f = -new.k * (distance - (self.r + new.r))
         return f * del_x / distance, f * del_y / distance
 
-    def __wall_force(self, w, b):
-        pass
+    def __wall_force(self, wall, obj):
+        return -self.k * abs(wall - obj)
 
     def wall_collide(self, wall):
         k_wall = 1000
         f = self.__wall_force(self.x, 0)
+        return f / self.m
+
+
+class wall(ball):
+    r = 0
+    k = 1000
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 # -----------------
-ball1 = ball(100, 100, 20, red, 2, 3, 1)
-ball2 = ball(100, 121, 20, blue, -1, -3, 1)
-ball1.draw_ball()
-ball2.draw_ball()
+ball1 = ball(50, 50, 20, red, 1, 2, 1)
+ball2 = ball(70, 70, 25, blue, 3, 4, 1)
+ball3 = ball(90, 90, 30, blue, 5, 6, 1)
+ball4 = ball(120, 121, 10, blue, 7, 8, 1)
+ball5 = ball(160, 160, 15, blue, 9, 10, 1)
+objects = [ball1, ball2, ball3, ball4, ball5]
+
+wall_top = wall(None, 0)
+wall_down = wall(None, 600)
+wall_left = wall(0, None)
+wall_right = wall(600, None)
+
+for obj in objects:
+    obj.draw_ball()
 pygame.display.update()
+
 cont = True
 dt = 0.001
 while cont:
+    for i, obj in enumerate(objects):
+        for new_obj in range(i, len(objects)):
+            if obj.collision_detection(new_obj):
+                f1x, f1y = obj.force_balls(new_obj)
+                f2x, f2y = -f1x, -f1y
+                ax = Fx / obj.m
+                ay = Fy / obj.m
+                obj.vx += ax * dt
+                obj.vy += ay * dt
+                obj.x += dt * obj.vx
+                obj.y += dt * obj.vy
+        if obj.y <= obj.r:
+            wall_top.x = obj.x
+            Fx, Fy = obj.force_balls(wall_top)
+            ax = Fx / obj.m
+            ay = Fy / obj.m
+            obj.vx += ax * dt
+            obj.vy += ay * dt
+            obj.x += dt * obj.vx
+            obj.y += dt * obj.vy
+        elif obj.y >= 600 - obj.r:
+            wall_down.x = obj.x
+            Fx, Fy = obj.force_balls(wall_down)
+            ax = Fx / obj.m
+            ay = Fy / obj.m
+            obj.vx += ax * dt
+            obj.vy += ay * dt
+            obj.x += dt * obj.vx
+            obj.y += dt * obj.vy
+        if obj.x <= obj.r:
+            wall_left.y = obj.y
+            Fx, Fy = obj.force_balls(wall_left)
+            ax = Fx / obj.m
+            ay = Fy / obj.m
+            obj.vx += ax * dt
+            obj.vy += ay * dt
+            obj.x += dt * obj.vx
+            obj.y += dt * obj.vy
+        elif obj.x >= 600 - obj.r:
+            wall_right.y = obj.y
+            Fx, Fy = obj.force_balls(wall_right)
+            ax = Fx / obj.m
+            ay = Fy / obj.m
+            obj.vx += ax * dt
+            obj.vy += ay * dt
+            obj.x += dt * obj.vx
+            obj.y += dt * obj.vy
 
-    if ball1.collision_detection(ball2):
-        ball1.draw_ball()
-        ball2.draw_ball()
-        pygame.display.update()
-        f1x, f1y = ball1.force_balls(ball2)
-        f2x, f2y = -f1x, -f1y
-        a1x = f1x / ball1.m
-        a2x = f2x / ball2.m
-        a1y = f1y / ball1.m
-        a2y = f2y / ball2.m
-        ball1.vx += a1x * dt
-        ball1.vy += a1y * dt
-        ball2.vx += a2x * dt
-        ball2.vy += a2y * dt
-        ball1.x += dt * ball1.vx
-        ball2.x += dt * ball2.vx
-        ball1.y += dt * ball1.vy
-        ball2.y += dt * ball2.vy
-        sc.fill(black)
-    ball1.x += dt * ball1.vx
-    ball2.x += dt * ball2.vx
-    ball1.y += dt * ball1.vy
-    ball2.y += dt * ball2.vy
-    ball1.draw_ball()
-    ball2.draw_ball()
+    sc.fill(black)
+    for obj in objects:
+        obj.draw_ball()
     pygame.display.update()
-
-    # if x > 600:
-    #     vx = -vx
-    # if y > 600:
-    #     vy = -vy
-    # if x < 0:
-    #     vx = -vx
-    # if y < 0:
-    #     vy = -vy
-    # draw_circle(yellow, x, y, r)
-    # pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -114,5 +146,4 @@ while cont:
                 cont = False
         if event.type == QUIT:
             cont = False
-    sc.fill(black)
 pygame.quit()
