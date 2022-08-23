134b3b88f3c3ee865e8bf9ca438c4dde6b6d4a25
mohbarati <mohbarati14@gmail.com>
2022-08-23 21:39:53 +0430

    comment/uncomment states of the matter

--- gas.py
+++ gas.py
@@ -1,44 +1,33 @@
 from math import sqrt
-
+from random import uniform, choices
 import pygame
 from pygame.locals import *
 
 pygame.init()
-sc = pygame.display.set_mode((600, 600))
-red = (255, 0, 0)
-blue = (100, 100, 255)
-yellow = (255, 255, 0)
-white = (255, 255, 255)
-black = (30, 60, 30)
-sc.fill(black)
+width, height = 600, 600
+sc = pygame.display.set_mode((width, height))
+color_range = range(50, 200)
+white = (250, 230, 240)
+sc.fill(white)
 
 
 class ball:
-    """Making instances of a ball and its atributes
-    """
-
-    k = 1000
-
-    def __init__(self, x, y, r, color, vx, vy, m):
-        self.x = x
-        self.y = y
-        self.r = r
-        self.color = color
-        self.vx = vx
-        self.vy = vy
-        self.m = m
+    """Making instances of a ball and its atributes"""
 
     def __distance(self, x, y):
-        return sqrt(x ** 2 + y ** 2)
+        return sqrt(x**2 + y**2)
 
     def draw_ball(self):
         pygame.draw.circle(sc, self.color, (self.x, self.y), self.r)
 
-    def erase_ball(self):
-        pygame.draw.circle(sc, black, (self.x, self.y), self.r)
-
     def collision_detection(self, new):
-        if self.__distance((self.x) - (new.x), (self.y) - (new.y),) <= self.r + new.r:
+        if (
+            self.__distance(
+                (self.x) - (new.x),
+                (self.y) - (new.y),
+            )
+            <= self.r + new.r
+        ):
             return True
         return False
 
@@ -49,46 +38,81 @@ class ball:
         f = -new.k * (distance - (self.r + new.r))
         return f * del_x / distance, f * del_y / distance
 
-    def __wall_force(self, wall, obj):
-        return -self.k * abs(wall - obj)
-
-    def wall_collide(self, wall):
-        k_wall = 1000
-        f = self.__wall_force(self.x, 0)
-        return f / self.m
-
 
 class wall(ball):
     r = 0
-    k = 1000
+    k = 10000
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
-# -----------------
-ball1 = ball(50, 50, 20, red, 10, 2, 1)
-ball2 = ball(70, 70, 25, blue, 30, 4, 1)
-ball3 = ball(90, 90, 30, blue, 50, 6, 1)
-ball4 = ball(120, 121, 10, blue, 7, 80, 1)
-ball5 = ball(160, 160, 15, blue, 9, 10, 1)
-objects = [ball1, ball2, ball3, ball4, ball5]
+# -----------------Solid
+r = 33
+m = 1
+k = 1000
+pos = []
+for i in range(r, width, 2 * r):
+    for j in range(r, height, 2 * r):
+        pos.append((i, j))
+number_of_balls = len(pos)
+ball_list = [ball() for _ in range(number_of_balls)]
+for i, item in enumerate(ball_list):
+    item.x, item.y = pos[i]
+    item.k = k
+    item.r = r
+    item.m = m
+    item.vx = uniform(-50, 50)
+    item.vy = uniform(-50, 50)
+    item.color = choices(color_range, k=3)
+# ------------------- liquid
+# r = 25
+# m = 1
+# k = 10
+# number_of_balls = 200
+# ball_list = [ball() for _ in range(number_of_balls)]
+# for item in ball_list:
+#     item.k = k
+#     item.r = r
+#     item.m = m
+#     item.x = uniform(0 + r, width - r)
+#     item.y = uniform(0 + r, height - r)
+#     item.vx = 0
+#     item.vy = 0
+#     item.color = choices(color_range, k=3)
+# ------------------------gas
+# r = 20
+# m = 1
+# k = 100
+# number_of_balls = 100
+# ball_list = [ball() for _ in range(number_of_balls)]
+# for item in ball_list:
+#     item.k = k
+#     item.r = r
+#     item.m = m
+#     item.x = uniform(0 + r, width - r)
+#     item.y = uniform(0 + r, height - r)
+#     item.vx = uniform(-3, 3)
+#     item.vy = uniform(-3, 3)
+#     item.color = choices(color_range, k=3)
+# ------------------------------
 
+# walls
 wall_top = wall(None, 0)
-wall_down = wall(None, 600)
+wall_down = wall(None, height)
 wall_left = wall(0, None)
-wall_right = wall(600, None)
+wall_right = wall(width, None)
 
-for obj in objects:
+for obj in ball_list:
     obj.draw_ball()
 pygame.display.update()
-
-cont = True
 dt = 0.01
+cont = True
+
 while cont:
-    for i, obj in enumerate(objects):
-        for new_obj in objects[i + 1 :]:
+    for i, obj in enumerate(ball_list):
+        for new_obj in ball_list[i + 1 :]:
             if obj.collision_detection(new_obj):
                 f1x, f1y = obj.force_balls(new_obj)
                 f2x, f2y = -f1x, -f1y
@@ -98,15 +122,8 @@ while cont:
                 a2y = f2y / new_obj.m
                 obj.vx += a1x * dt
                 obj.vy += a1y * dt
-                obj.x += dt * obj.vx
-                obj.y += dt * obj.vy
                 new_obj.vx += a2x * dt
                 new_obj.vy += a2y * dt
-                new_obj.x += dt * new_obj.vx
-                new_obj.y += dt * new_obj.vy
-            else:
-                obj.x += dt * obj.vx
-                obj.y += dt * obj.vy
         if obj.y <= obj.r:
             wall_top.x = obj.x
             Fx, Fy = obj.force_balls(wall_top)
@@ -114,17 +131,13 @@ while cont:
             ay = Fy / obj.m
             obj.vx += ax * dt
             obj.vy += ay * dt
-            obj.x += dt * obj.vx
-            obj.y += dt * obj.vy
-        elif obj.y >= 600 - obj.r:
+        elif obj.y >= height - obj.r:
             wall_down.x = obj.x
             Fx, Fy = obj.force_balls(wall_down)
             ax = Fx / obj.m
             ay = Fy / obj.m
             obj.vx += ax * dt
             obj.vy += ay * dt
-            obj.x += dt * obj.vx
-            obj.y += dt * obj.vy
         if obj.x <= obj.r:
             wall_left.y = obj.y
             Fx, Fy = obj.force_balls(wall_left)
@@ -132,20 +145,17 @@ while cont:
             ay = Fy / obj.m
             obj.vx += ax * dt
             obj.vy += ay * dt
-            obj.x += dt * obj.vx
-            obj.y += dt * obj.vy
-        elif obj.x >= 600 - obj.r:
+        elif obj.x >= width - obj.r:
             wall_right.y = obj.y
             Fx, Fy = obj.force_balls(wall_right)
             ax = Fx / obj.m
             ay = Fy / obj.m
             obj.vx += ax * dt
             obj.vy += ay * dt
-            obj.x += dt * obj.vx
-            obj.y += dt * obj.vy
-
-    sc.fill(black)
-    for obj in objects:
+        obj.x += dt * obj.vx
+        obj.y += dt * obj.vy
+    sc.fill(white)
+    for obj in ball_list:
         obj.draw_ball()
     pygame.display.update()
 
